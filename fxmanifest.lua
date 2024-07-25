2267e79bf71f62ce45d449bab668b40cfba6df8b
Bacon_Space <166666971+BaconSpaces@users.noreply.github.com>
2024-07-25 14:38:41 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bacon_Space'
+description 'BSRP FreeRoam - Tow Script'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+dependencies {
+    'qb-core',
+    'progressbar',
+    'oxlib'
+}
